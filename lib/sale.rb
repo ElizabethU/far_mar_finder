cdf54890cf7caf3386f2ef3d008efe7dabaad97d
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2013-11-11 15:09:43 -0800

    Added classes

---
+++ lib/sale.rb
@@ -0,0 +1,15 @@
+class Sales
+  # attr_reader :name, :address #etc
+
+  # def initialize(array)
+  #   @name = array[1]
+  #   @address = array[2]
+  # end
+
+  # def self.all
+  #   CSV.read("./support/markets.csv").map do |array|
+  #     Market.new(array)
+  #   end
+  # end
+
+end
